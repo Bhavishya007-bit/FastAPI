65a504139249146264f25670fc491ff7908bf5ef
Bhavishya Gaur <bhavishyagaur6@gmail.com>
2026-08-30 20:47:43 +0530

    Enhance Patient model with email and LinkedIn URL fields, and make allergies optional

--- Pydantic/pydantic_why.py
+++ Pydantic/pydantic_why.py
@@ -1,14 +1,16 @@
-from pydantic import BaseModel
-from typing import List, Dict
+from pydantic import BaseModel, EmailStr, AnyUrl
+from typing import List, Dict, Optional
 
 #Pydantic model
 class Patient(BaseModel): # We have to always pass BaseModel inside the class
      #Schema defined
      name : str
+     email : EmailStr
+     linkedin_url : AnyUrl
      age : int
      weight : float
      married : bool
-     allergies : List[str]
+     allergies : Optional[List[str]] = None
      contact_details : Dict[str,str]
 
 def insert_patient_data(patient : Patient):
@@ -24,7 +26,7 @@ def update_patient_data(patient : Patient):
     print('updated')
 
 
-patient_info = {'name': 'Bhavishya', 'age':20, 'weight': 76.55, 'married': False, 'allergies': ['pollen', 'dust'], 'contact_details':{'email':'abc@gmail.com', 'phone':'789541288'}}
+patient_info = {'name': 'Bhavishya', 'email':'abc@gmail.com','linkedin_url':'http://linkedin.com/1322','age':20, 'weight': 76.55, 'married': False, 'allergies': ['pollen', 'dust'], 'contact_details':{'email':'abc@gmail.com', 'phone':'789541288'}}
 
 patient1 = Patient(**patient_info)  # Here we have used ** to unpack the dictionary
 
