ca79bf631f7428725972ced147381c955841fc92
Bhavishya Gaur <bhavishyagaur6@gmail.com>
2026-08-30 18:32:22 +0530

    Add Pydantic model for patient data management

---
+++ Pydantic/pydantic_why.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import List, Dict
+
+#Pydantic model
+class Patient(BaseModel): # We have to always pass BaseModel inside the class
+     #Schema defined
+     name : str
+     age : int
+     weight : float
+     married : bool
+     allergies : List[str]
+     contact_details : Dict[str,str]
+
+def insert_patient_data(patient : Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print('inserted')
+
+def update_patient_data(patient : Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print('updated')
+
+
+patient_info = {'name': 'Bhavishya', 'age':20, 'weight': 76.55, 'married': False, 'allergies': ['pollen', 'dust'], 'contact_details':{'email':'abc@gmail.com', 'phone':'789541288'}}
+
+patient1 = Patient(**patient_info)  # Here we have used ** to unpack the dictionary
+
+insert_patient_data(patient1)
+
+update_patient_data(patient1)  # pydantic is smart enough and converted it into integer
