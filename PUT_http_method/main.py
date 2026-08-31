f804b37d08936c5ded4fbeb36c3c3aa2bfc4e536
Bhavishya Gaur <bhavishyagaur6@gmail.com>
2026-08-31 22:01:06 +0530

    Add BMI calculation and sorting functionality for patients

--- PUT_http_method/main.py
+++ PUT_http_method/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path, HTTPException, Query
-from pydantic import BaseModel, Field
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
 from typing import Annotated, Literal
 import json
 
@@ -8,8 +9,11 @@ app = FastAPI()
 def load_data():
     with open('patient.json', 'r') as f:
         data = json.load(f)
-
     return data
+
+def save_data(data):
+    with open('patient.json', 'w') as f:
+        json.dump(data, f)
 
 class Patient(BaseModel):
 
@@ -20,4 +24,63 @@ class Patient(BaseModel):
     gender : Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
     height : Annotated[float, Field(..., gt = 0, description='Height of the patient in meter')]
     weight : Annotated[float, Field(..., gt = 0, description='Weight of ther patient in kg')]
-    
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+
+        return bmi
+
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if self.bmi <18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal weight'
+        elif self.bmi < 30:
+            return 'OverWeight'
+        else:
+            return 'Obese'
+
+@app.get("/")
+def hello():
+    return {'message': 'Patient data management system'}
+
+@app.get("/about")
+def about():
+    return {
+        'message': 'Hi this is my first FastAPI code thank you hehehe'
+    }
+
+@app.get('/sort')
+def sort_patient(
+    sort_by: str = Query(..., description='Sort on the basis of height, weight, or bmi'),
+    order: str = Query('asc', description='Sort in asc or desc order')
+):
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(
+            status_code=400,
+            detail=f'Invalid field select from {valid_fields}'
+        )
+
+    if order not in ['asc', 'desc']:
+        raise HTTPException(
+            status_code=400,
+            detail='Invalid order select between asc or desc'
+        )
+
+    data = load_data()
+
+    sort_order = True if order == 'desc' else False
+
+    sorted_data = sorted(
+        data.values(),
+        key=lambda x: x.get(sort_by, 0),
+        reverse=sort_order
+    )
+
+    return sorted_data
